f9367a58b2ab62021732dc3561fa3f9b2c5c0cd0
VarunaLex <lex.varuna@gmail.com>
2020-09-12 21:55:15 +0530

    Gulp sass config

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const { dest, parallel, series, src, task, watch } = require('gulp');
+
+// CSS
+const sass = require('gulp-sass');
+
+// Utils
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Style paths
+const styleSrc = 'src/scss/style.scss';
+const styleDist = './dist/css/';
+
+function style(done) {
+  src(styleSrc)
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        errLogToConsole: true, //
+        outputStyle: 'compressed',
+      }),
+    )
+    .on('error', console.error.bind(console))
+    .pipe(rename({ suffix: '.min' })) // or extname: '.min.css'
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest(styleDist));
+
+  done();
+}
+
+// Register tasks
+task('style', style); // gulp style
+
+task('default', parallel(style)); // gulp
